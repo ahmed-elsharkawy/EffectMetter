bd520ca8eda28f74dd67ac9ce3f4237c24d794ee
ahmed-elsharkawy <engelsharkawy9@gmail.com>
2022-01-11 19:38:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $(".art1 .down .father1").mouseenter(function(){
+        $(".art1 .down .father1 .back").animate({
+            top:'+=200px'
+        },"slow");
+        $(".art1 .down .father1 .hover").animate({
+            bottom:'-=200px'
+        },"slow");
+    });
+        $(".art1 .down .father1").mouseleave(function(){
+        $(".art1 .down .father1 .back").animate({
+            top:'-=200px'
+        },"slow");
+        $(".art1 .down .father1 .hover").animate({
+            bottom:'+=200px'
+        },"slow");
+    });   
+    
+    $(".art1 .down .father2").mouseenter(function(){
+        $(".art1 .down .father2 .back").animate({
+            top:'+=200px'
+        },"slow");
+        $(".art1 .down .father2 .hover").animate({
+            bottom:'-=200px'
+        },"slow");
+    });
+        $(".art1 .down .father2").mouseleave(function(){
+        $(".art1 .down .father2 .back").animate({
+            top:'-=200px'
+        },"slow");
+        $(".art1 .down .father2 .hover").animate({
+            bottom:'+=200px'
+        },"slow");
+    });
+    
+    $(".art1 .down .father3").mouseenter(function(){
+        $(".art1 .down .father3 .back").animate({
+            top:'+=200px'
+        },"slow");
+        $(".art1 .down .father3 .hover").animate({
+            bottom:'-=200px'
+        },"slow");
+    });
+        $(".art1 .down .father3").mouseleave(function(){
+        $(".art1 .down .father3 .back").animate({
+            top:'-=200px'
+        },"slow");
+        $(".art1 .down .father3 .hover").animate({
+            bottom:'+=200px'
+        },"slow");
+    });
+    
+    $(".art1 .down .father4").mouseenter(function(){
+        $(".art1 .down .father4 .back").animate({
+            top:'+=200px'
+        },"slow");
+        $(".art1 .down .father4 .hover").animate({
+            bottom:'-=200px'
+        },"slow");
+    });
+        $(".art1 .down .father4").mouseleave(function(){
+        $(".art1 .down .father4 .back").animate({
+            top:'-=200px'
+        },"slow");
+        $(".art1 .down .father4 .hover").animate({
+            bottom:'+=200px'
+        },"slow");
+    });
+    
+})
